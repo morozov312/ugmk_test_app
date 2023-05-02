86be764654ef0e1932112c73bb3370df870828bf
morozov312 <alexander.morozov312@gmail.com>
2023-05-03 02:30:52 +0700

    chore(src/axios) setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './nav/AppRouter.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
+);
